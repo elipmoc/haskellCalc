056eb5157fae61871f02e24eda547c4d82c84b1a
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-11-04 17:15:53 +0900

    計算失敗をうまくあつかえるように

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,24 @@
 module Main where
 
+import Control.Monad
+
 readMaybe :: (Read a) => String -> Maybe a
 readMaybe st = case reads st of [(x,"")] -> Just x
                                 _ -> Nothing
 
-solveRPN :: String -> Double
-solveRPN =head . foldl foldingFunction [] . words
-    where 
-        foldingFunction (x:y:ys) "*" = (y*x):ys
-        foldingFunction (x:y:ys) "/" = (y/x):ys
-        foldingFunction (x:y:ys) "mul" = (y*x):ys
-        foldingFunction (x:y:ys) "div" = (y/x):ys
-        foldingFunction (x:y:ys) "+" = (y+x):ys
-        foldingFunction (x:y:ys) "-" = (y-x):ys
-        foldingFunction xs numberString = read numberString:xs
+foldingFunction :: [Double] -> String -> Maybe [Double]
+foldingFunction (x:y:ys) "*" = return $ (y*x):ys
+foldingFunction (x:y:ys) "/" = return $ (y/x):ys
+foldingFunction (x:y:ys) "mul" = return $ (y*x):ys
+foldingFunction (x:y:ys) "div" = return $ (y/x):ys
+foldingFunction (x:y:ys) "+" = return $ (y+x):ys
+foldingFunction (x:y:ys) "-" = return $ (y-x):ys
+foldingFunction xs numberString = liftM (:xs) (readMaybe numberString)
+
+solveRPN :: String -> Maybe Double
+solveRPN st =do 
+    [result]<- foldM foldingFunction [] (words st)
+    return result
 
 main :: IO ()
 main = do 
