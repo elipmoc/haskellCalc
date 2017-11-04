d08661213f91ab99ed74613ad2d23cc7d7b3a5c7
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-11-04 16:08:34 +0900

    add mul,div

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,8 @@ solveRPN =head . foldl foldingFunction [] . words
     where 
         foldingFunction (x:y:ys) "*" = (y*x):ys
         foldingFunction (x:y:ys) "/" = (y/x):ys
+        foldingFunction (x:y:ys) "mul" = (y*x):ys
+        foldingFunction (x:y:ys) "div" = (y/x):ys
         foldingFunction (x:y:ys) "+" = (y+x):ys
         foldingFunction (x:y:ys) "-" = (y-x):ys
         foldingFunction xs numberString = read numberString:xs
